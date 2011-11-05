82f51afb04a5f1d261a046a8748e7a92b77cc57d
francisco.guiraldelli@gmail.com <francisco.guiraldelli@gmail.com@6fb5fa6b-224d-39bc-2600-6e9b45507570>
2011-11-05 20:56:04 +0000

    Estrutura inicial do arquivos do trabalho de ED2

---
+++ trunk/Huffman/huffman.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   huffman.h
+ * Author: fguira
+ *
+ * Created on 5 de Novembro de 2011, 18:51
+ */
+
+#ifndef HUFFMAN_H
+#define	HUFFMAN_H
+
+#include <cstdlib>
+#include <cstdio>
+#include <cctype>
+#include <ctime>
+
+#endif	/* HUFFMAN_H */
+
---
+++ trunk/Huffman/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: fguira
+ *
+ * Created on 5 de Novembro de 2011, 18:48
+ */
+
+#include "huffman.h"
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
