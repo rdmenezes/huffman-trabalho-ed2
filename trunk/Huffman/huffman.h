74d73cc61625ce45ae41d45044023ecf43bb5afe
francisco.guiraldelli@gmail.com <francisco.guiraldelli@gmail.com@6fb5fa6b-224d-39bc-2600-6e9b45507570>
2011-11-06 03:08:01 +0000

    Inclusão de algumas prováveis classes necessárias para o programa e atribuições e métodos relativos a cada um.(rascunho)

--- trunk/Huffman/huffman.h
+++ trunk/Huffman/huffman.h
@@ -12,6 +12,50 @@
 #include <cstdio>
 #include <cctype>
 #include <ctime>
+#include <cstring>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+class Arquivo{
+private:
+    FILE* arquivoOrigem;
+    FILE* arquivoDestino;
+    int tamanhoArquivoOrigem;
+    int tamanhoArquivoDestino;
+    std::vector<int>* frequenciaCaracteres;
+    std::vector<int>* quantidadeBits;
+    
+public:
+    void leArquivo(int* argc, char** argv);
+    void contaCaracteres(FILE *f);
+    
+};
+
+class Estatistica{
+private:
+    int taxaCompactacao;
+    int mediaBits;
+    int tempoConsumido;
+    
+public:
+};
+
+class Huffman{
+private:
+    
+public:
+    void encodeHuffman();
+    void decodeHuffman();
+     
+};
+
+
+
+
 
 #endif	/* HUFFMAN_H */
 
