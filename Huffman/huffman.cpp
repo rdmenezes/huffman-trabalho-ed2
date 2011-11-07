e485618be96f5dcd2c7599ff22d21fca6fc9f57e
francisco.guiraldelli@gmail.com <francisco.guiraldelli@gmail.com@6fb5fa6b-224d-39bc-2600-6e9b45507570>
2011-11-07 02:43:54 +0000

    Inserção de algumas funções para verificar as situações de contorno ocorridas no programa

--- Huffman/huffman.cpp
+++ Huffman/huffman.cpp
@@ -1,3 +1,18 @@
 #include "huffman.h"
 
+using namespace std;
 
+Arquivo::Arquivo(): tamanhoVetorAscii(256){
+    frequenciaCaracteres = new int[tamanhoVetorAscii];
+}
+
+bool verificaArquivo(char* nomeArquivo){
+    FILE* arquivo;
+    if((arquivo = fopen(nomeArquivo, "r")) == NULL){
+        cout << "Arquivo não encontrado!" << endl;
+        //fclose(arquivo);
+        return 1;
+    }
+    fclose(arquivo);
+    return 0;
+}
--- Huffman/main.cpp
+++ Huffman/main.cpp
@@ -13,7 +13,17 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-
+    if (argc < 3 || argc >4){
+       cout << "Uso: huffman [opção] [arq_origem] [arq_destino]" << endl;
+       return 1;
+    }
+    if(verificaArquivo(argv[2]))
+        return 1;
+    cout << argc << endl;
+    while(argc-- > 0){
+        cout << argv[argc] << endl;
+    }
+    
     return 0;
 }
 
